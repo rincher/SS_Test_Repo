dcaf00f26f3d4f7b89cfddd32bb099a5fc4d829b
hdyoon <skymegod@gmail.com>
2023-03-13 14:00:21 +0900

    test

---
+++ src/main/java/com/example/AWSkeyExposed/AwSkeyExposedApplication.java
@@ -0,0 +1,13 @@
+package com.example.AWSkeyExposed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwSkeyExposedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwSkeyExposedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AWSkeyExposed/AwSkeyExposedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AWSkeyExposed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwSkeyExposedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
